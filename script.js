d3229cf0db5e279f95d9e2efcf925f281d6bfca4
codeesura <120671243+codeesura@users.noreply.github.com>
2023-06-07 20:05:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const factory_contract_syncswap = "0xf2FD2bc2fBC12842aAb6FbB8b1159a6a83E72006";
 const factory_abi_syncswap = require("./ABI/factory_abi_syncswap.json");
 const pool_abi_syncswap = require("./ABI/pool_abi_syncswap.json");
 
+const ERC20_abi = require("./ABI/erc20_abi.json");
+
 const WETH_MUTEIO = "0x294cB514815CAEd9557e6bAA2947d6Cf0733f014";
 const WETH_SYNCSWAP = "0x20b28b1e4665fff290650586ad76e977eab90c5d";
 
@@ -14,7 +16,7 @@ const DAI_ADDRESS = "0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b";
 const LINK_ADDRESS = "0x40609141Db628BeEE3BfAB8034Fc2D8278D0Cc78";
 const USDC_ADDRESS = "0x0faF6df7054946141266420b43783387A78d82A9";
 const WBTC_ADDRESS = "0x0BfcE1D53451B4a8175DD94e6e029F7d8a701e9c";
-const liste = [DAI_ADDRESS,LINK_ADDRESS,USDC_ADDRESS,WBTC_ADDRESS];
+const liste = [USDC_ADDRESS,DAI_ADDRESS,LINK_ADDRESS,WBTC_ADDRESS];
 const provider = new ethers.providers.JsonRpcProvider(`https://testnet.era.zksync.dev`);
 
 const factory_contract_mute_interface = new ethers.Contract(factory_contract_muteio, factory_abi_muteio, provider);
@@ -26,6 +28,18 @@ async function main() {
         const getir_mute = await factory_contract_mute_interface.getPair(WETH_MUTEIO,eleman,0,{ blockTag: blockNumber });
         const reserves_mute_contract = new ethers.Contract(getir_mute, pool_abi_muteio, provider);
         const reserves_mute = await reserves_mute_contract.getReserves({ blockTag: blockNumber });
+        const reserves_0_contract_ = await reserves_mute_contract.token0({ blockTag: blockNumber });
+        const reserves_0_contract = new ethers.Contract(reserves_0_contract_, ERC20_abi, provider);
+        const reserves_0_name = await reserves_0_contract.name({ blockTag: blockNumber });
+        const reserves_0_symbol = await reserves_0_contract.symbol({ blockTag: blockNumber });
+        const reserves_0_decimal = await reserves_0_contract.decimals({ blockTag: blockNumber });
+
+        const reserves_1_contract_ = await reserves_mute_contract.token1({ blockTag: blockNumber });
+        const reserves_1_contract = new ethers.Contract(reserves_1_contract_, ERC20_abi, provider);
+        const reserves_1_name = await reserves_1_contract.name({ blockTag: blockNumber });
+        const reserves_1_symbol = await reserves_1_contract.symbol({ blockTag: blockNumber });
+        const reserves_1_decimal = await reserves_1_contract.decimals({ blockTag: blockNumber });
+
         console.log('Mute, token0_reserves = '+reserves_mute[0]+" -- token1_reserves = "+reserves_mute[1]);
 
         const getir_syncswap = await factory_contract_syncswap_interface.getPool(WETH_SYNCSWAP,eleman,{ blockTag: blockNumber });
